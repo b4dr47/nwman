6a60cc839280448c510e9fb18d46ff17c131ed21
b4dr47 <me@b4dr47.dev>
2026-04-14 13:42:55 +0800

    add gif & audio

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import gugugaga from "$lib/assets/gugugaga.gif";  
+  import audio from "$lib/assets/audio.mp3";
+</script>
 <div class="min-h-screen flex items-center justify-center bg-gray-900">
-<p class="texl-2xl text-gray-200">u arrived here</p>
+<img src={gugugaga} alt="gugugaga!">
+<audio control autoplay>
+<source src={audio} type="audio/mp3">
+</audio>
 </div>
+
